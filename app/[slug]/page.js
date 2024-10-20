13f45004155ee1de0c3658300f8690486d0be7f5
sammyyakk <sammyyakk@gmail.com>
2024-10-21 05:10:50 +0530

    More errors fixed

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,16 +1,16 @@
-import MarkdownPage from '../components/MarkdownPage'
+import fs from 'fs';
+import path from 'path';
+import MarkdownPage from '../components/MarkdownPage';
 
 export default function DynamicPage({ params }) {
-  return <MarkdownPage filePath={`pages/${params.slug}.md`} />
+  return <MarkdownPage filePath={`pages/${params.slug}.md`} />;
 }
 
 export async function generateStaticParams() {
-  const fs = require('fs')
-  const path = require('path')
-  const contentDir = path.join(process.cwd(), 'content', 'pages')
-  const files = fs.readdirSync(contentDir)
-  
+  const contentDir = path.join(process.cwd(), 'content', 'pages');
+  const files = fs.readdirSync(contentDir);
+
   return files.map(file => ({
     slug: file.replace(/\.md$/, '')
-  }))
+  }));
 }
